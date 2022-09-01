ba84154f817aac0812c725253e71ed1714325575
Luis Cruz <luisccc99@gmail.com>
2022-08-31 21:21:20 -0600

    Change data type to avoid overflow error

--- QueueUsingTwoStacks/Solution.java
+++ QueueUsingTwoStacks/Solution.java
@@ -5,32 +5,34 @@ public class Solution {
   public static final int ENQUEUE = 1;
   public static final int DEQUEUE = 2;
   public static final int PRINT_FIRST_ELEMENT = 3;
-  
+
+  private static void move(Stack<Long> from, Stack<Long> to) {
+    while (!from.isEmpty()) {
+      to.push(from.pop());
+    }
+  }
+
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int numQueries = Integer.parseInt(in.nextLine().trim());
-    
-    Stack<Integer> s1 = new Stack<Integer>();
-    Stack<Integer> s2 = new Stack<Integer>();
-    
+
+    Stack<Long> s1 = new Stack<Long>();
+    Stack<Long> s2 = new Stack<Long>();
+
     for (int i = 0; i < numQueries; i++) {
       String line = in.nextLine();
       int query = Integer.parseInt(line.split(" ")[0]);
       if (query == Solution.ENQUEUE) {
-        int x = Integer.parseInt(line.split(" ")[1]);
+        long x = Long.parseLong(line.split(" ")[1]);
         s1.push(x);
       } else if (query == Solution.DEQUEUE) {
-        if (!s2.isEmpty()) {
-          s2.pop();
-        } else {
-          s2.addAll(s1);
-          s1.clear();
-          s2.pop();
-        }
+        move(s1, s2);
+        s2.pop();
       } else if (query == Solution.PRINT_FIRST_ELEMENT) {
+        move(s1, s2);
         System.out.println(s2.peek());
       }
-    } 
+    }
     in.close();
   }
 }
