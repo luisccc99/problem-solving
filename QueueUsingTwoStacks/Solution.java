28c75c8ee8f052d2cd08925d73add1dad64ddad0
Luis Cruz <luisccc99@gmail.com>
2022-09-01 22:03:01 -0600

    Solve queue with two stacks problem

--- QueueUsingTwoStacks/Solution.java
+++ QueueUsingTwoStacks/Solution.java
@@ -13,26 +13,34 @@ public class Solution {
   }
 
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int numQueries = Integer.parseInt(in.nextLine().trim());
+    Scanner scanner = new Scanner(System.in);
+    int numQueries = Integer.parseInt(scanner.nextLine().trim());
 
     Stack<Long> s1 = new Stack<Long>();
     Stack<Long> s2 = new Stack<Long>();
 
     for (int i = 0; i < numQueries; i++) {
-      String line = in.nextLine();
+      String line = scanner.nextLine();
       int query = Integer.parseInt(line.split(" ")[0]);
       if (query == Solution.ENQUEUE) {
         long x = Long.parseLong(line.split(" ")[1]);
-        s1.push(x);
+        if (s1.isEmpty() && s2.isEmpty()) {
+          s2.push(x);
+        } else {
+          s1.push(x);
+        }
       } else if (query == Solution.DEQUEUE) {
-        move(s1, s2);
+        if (s2.isEmpty()) {
+          move(s1, s2);
+        }
         s2.pop();
       } else if (query == Solution.PRINT_FIRST_ELEMENT) {
-        move(s1, s2);
+        if (s2.isEmpty()) {
+          move(s1, s2);
+        }
         System.out.println(s2.peek());
       }
     }
-    in.close();
+    scanner.close();
   }
 }
