806343752d9037b7c224add97b7378635854ce5c
Luis Cruz <luisccc99@gmail.com>
2022-09-03 22:10:13 -0600

    Start solving balanced brackets problem

---
+++ BalancedBrackets/Solution.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+
+public class Solution {
+
+  /**
+   * 
+   * @param s string of brackets: (, ), {, }, [, ]
+   * @return YES if string of brackets is balanced or NO if it's not
+   */
+  public static String isBalanced(String s) {
+    if (s.length() < 2) {
+      return "NO";
+    }
+    
+    Stack<Character> brackets = new Stack<>();
+    char[] closingBrackets = { ')', ']', '}' };
+
+    for (int i = 0; i < s.length(); i++) {
+      char current = s.charAt(i);
+      if (isIn(current, closingBrackets)) {
+        if (brackets.isEmpty()) {
+          return "NO";
+        }
+        char top = brackets.pop();
+        if (top == '(' && current == ')') {
+          continue;
+        } else if (top == '[' && current == ']') {
+          continue;
+        } else if (top == '{' && current == '}') {
+          continue;
+        } else {
+          return "NO";
+        }
+      } else {
+        brackets.push(current);
+      }
+    }
+    return "YES";
+  }
+
+  private static boolean isIn(char key, char[] arr) {
+    for (int i = 0; i < 0; i++) {
+      if (key == arr[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
